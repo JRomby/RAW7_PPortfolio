1114b6a897d4e422e6e628f4439f0313c01dec43
JRomby <37065173+JRomby@users.noreply.github.com>
2020-10-22 10:39:20 +0200

    Update Program.cs

--- Project Portfolio/Project Portfolio/Program.cs
+++ Project Portfolio/Project Portfolio/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿
+using System;
+using Npgsql;
 
 namespace Project_Portfolio
 {
-    class Program
+    class ConnectDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectionString =
+                 ""; //connection info for DB
+
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open(); //connect to DB
+
+            using var cmd = new NpgsqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = "select categoryid, categoryname from categories";
+
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"Id={reader.GetInt32(0)}, Name={reader.GetString(1)}");
+            }
         }
     }
 }
